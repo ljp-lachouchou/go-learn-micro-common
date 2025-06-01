d99e77122c986a6cfb360e1aa4c57e98a619af7d
aliyun8025300295-jM18Y <2950203059@qq.com>
2025-06-01 13:20:09 +0800

    修改监控模块代码

--- prometheus.go
+++ prometheus.go
@@ -8,10 +8,10 @@ import (
 )
 
 func PrometheusBoot(port int) {
-	http.Handle("/metrices", promhttp.Handler())
+	http.Handle("/metrics", promhttp.Handler())
 	//启动web服务
 	go func() {
-		err := http.ListenAndServe("0.0.0.0"+strconv.Itoa(port), nil)
+		err := http.ListenAndServe("0.0.0.0:"+strconv.Itoa(port), nil)
 
 		if err != nil {
 			log.Fatal("启动失败")
